3a875f3e6e253826198fa1cd9a279bc49286bb26
tom <tom@toms-MacBook-Pro-2016.local>
2020-02-26 20:57:40 -0500

    added async

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var imageResize = require('gulp-image-resize');
  
 gulp.task('resize', function () {
-  gulp.src('images/full/*.{jpg,png}')
+  return gulp.src('images/full/*.{jpg,png}')
     .pipe(imageResize({
       height : 300
     }))
